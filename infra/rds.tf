1009416d614921765d8bd7fd856928d09d1cc928
rodriguecyber <rodrirwigara@gmail.com>
2026-04-09 11:55:50 +0200

    correct sg

--- infra/rds.tf
+++ infra/rds.tf
@@ -1,10 +1,12 @@
 resource "aws_db_instance" "primary" {
-  identifier         = "fincorp-db-instance"
-  engine             = "postgres"
-  instance_class     = "db.t3.micro"
-  allocated_storage = 20
+  identifier          = "fincorp-db-instance"
+  engine              = "postgres"
+  instance_class      = "db.t3.micro"
+  allocated_storage   = 20
+  db_name             = "fincorp"
   publicly_accessible = true
-  username           = "rwigara"
-  password           = "yourpassword"
+  username            = "rwigara"
+  password            = "yourpassword"
+  vpc_security_group_ids = [aws_security_group.rds.id]
   skip_final_snapshot = true
 }
--- infra/sg.tf
+++ infra/sg.tf
@@ -1,16 +1,22 @@
+data "aws_vpc" "default" {
+  default = true
+}
+
 resource "aws_security_group" "rds" {
-  name = "rds_sg"
-  
+  name   = "rds_sg"
+  vpc_id = data.aws_vpc.default.id
+
   ingress {
     from_port = 5432
     to_port   = 5432
     protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
-  
+
   egress {
     from_port = 0
     to_port   = 0
     protocol  = "-1"
-  
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
